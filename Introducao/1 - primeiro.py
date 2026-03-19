378f5685c84701df9e49e435d3de5b90a11bc963
Marcelo <marvenpgo@gmail.com>
2026-03-19 08:03:57 -0300

    Atualizar

---
+++ Introducao/1 - primeiro.py
@@ -0,0 +1,9 @@
+print("Olá Mundo!\n") # o \n insere uma linha no final do print
+print("Aprendendo a linguagem Python!")
+
+# insere comentário na linha
+
+''' 
+Insere comentário de multiplas linhas
+
+'''
